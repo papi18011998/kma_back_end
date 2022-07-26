347a16a3f9ebbcd8fd6531ea9c271d5bf606e724
papi18011998 <papaibrahima98@gmail.com>
2022-07-26 12:29:16 +0000

    creation de la classe authority pour les roles

--- src/main/java/com/example/spring_jwt_get_arrays/Ressources/UserRessource.java
+++ src/main/java/com/example/spring_jwt_get_arrays/Ressources/UserRessource.java
@@ -1,16 +1,17 @@
 package com.example.spring_jwt_get_arrays.Ressources;
 
 import com.example.spring_jwt_get_arrays.domain.User;
+import com.example.spring_jwt_get_arrays.exception.domain.EmailExistException;
 import com.example.spring_jwt_get_arrays.exception.domain.ExceptionHandling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(path = {"/","/users"})
 public class UserRessource extends ExceptionHandling {
     @GetMapping("home")
-    public String showUser(){
-        return "Application works";
+    public String showUser() throws EmailExistException {
+        throw  new EmailExistException("This email is already taken");
     }
 }
--- src/main/java/com/example/spring_jwt_get_arrays/exception/domain/ExceptionHandling.java
+++ src/main/java/com/example/spring_jwt_get_arrays/exception/domain/ExceptionHandling.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.example.spring_jwt_get_arrays.domain.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,19 +14,20 @@ import org.springframework.security.authentication.LockedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.persistence.NoResultException;
 import java.io.IOException;
 import java.rmi.AccessException;
-import java.util.Arrays;
-import java.util.Locale;
+
 import java.util.Objects;
 
 import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
-public class ExceptionHandling {
+public class ExceptionHandling  implements ErrorController {
     private final  Logger LOGGER = LoggerFactory.getLogger(getClass());
     private static final String ACCOUNT_LOCKED = "Your account has been locked. Please contact administration";
     private static final String METHOD_IS_NOT_ALLOWED = "This request method is not allowed on this endpoint. Please send a '%s' request";
@@ -34,6 +36,7 @@ public class ExceptionHandling {
     private static final String ACCOUNT_DISABLED = "Your account has been disabled. If this is an error, please contact administration";
     private static final String ERROR_PROCESSING_FILE = "Error occurred while processing file";
     private static final String NOT_ENOUGH_PERMISSION = "You do not have enough permission";
+    public static final String ERROR_PATH= "/error";
 
     @ExceptionHandler(DisabledException.class)
     public ResponseEntity<HttpResponse> accountDisabledException(){
@@ -74,17 +77,21 @@ public class ExceptionHandling {
     public ResponseEntity<HttpResponse> userNotFoundException(UserNotFoundException exception) {
         return createHttpResponse(BAD_REQUEST, exception.getMessage());
     }
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<HttpResponse> urlNotfoundException(NoHandlerFoundException e) {
+        return createHttpResponse(BAD_REQUEST, "This page was not found");
+    }
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<HttpResponse> methodNotSupportedException(HttpRequestMethodNotSupportedException exception) {
         HttpMethod supportedMethod = Objects.requireNonNull(exception.getSupportedHttpMethods()).iterator().next();
         return createHttpResponse(METHOD_NOT_ALLOWED, String.format(METHOD_IS_NOT_ALLOWED, supportedMethod));
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<HttpResponse> internalServerErrorException(Exception exception) {
-        LOGGER.error(exception.getMessage());
-        return createHttpResponse(INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR_MSG);
-    }
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<HttpResponse> internalServerErrorException(Exception exception) {
+//        LOGGER.error(exception.getMessage());
+//        return createHttpResponse(INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR_MSG);
+//    }
 
 //    @ExceptionHandler(NotAnImageFileException.class)
 //    public ResponseEntity<HttpResponse> notAnImageFileException(NotAnImageFileException exception) {
@@ -98,6 +105,10 @@ public class ExceptionHandling {
         return createHttpResponse(NOT_FOUND, exception.getMessage());
     }
 
+    @RequestMapping(ERROR_PATH)
+    public ResponseEntity<HttpResponse> notFounf404() {
+        return createHttpResponse(NOT_FOUND,"There is no mapping for this URL");
+    }
     @ExceptionHandler(IOException.class)
     public ResponseEntity<HttpResponse> iOException(IOException exception) {
         LOGGER.error(exception.getMessage());
@@ -109,4 +120,5 @@ public class ExceptionHandling {
         HttpResponse httpResponse = new HttpResponse(httpStatus.value(),httpStatus, httpStatus.getReasonPhrase().toUpperCase(),message);
         return  new ResponseEntity<>(httpResponse,httpStatus);
     }
+
 }
